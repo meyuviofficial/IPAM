f919465b9ba305b9c7af7253400a81bf130f0325
Yuvi <yuvarajselva@hotmail.com>
2022-07-19 18:00:53 +0530

    feat: Added the feature to handle subnet masks

--- python/main.py
+++ python/main.py
@@ -1,6 +1,8 @@
+from multiprocessing.sharedctypes import Value
 import radix as r
 import pandas as pd
 import numpy as np
+import ipaddress as ip
 import time
 
 class IPAM:
@@ -14,44 +16,60 @@ class IPAM:
         # Self Overlap
         for index, row in self.data.iterrows():
             if self.data["INFOBLOX_TABLE"][index]:
-                new_node = left_Tree.search_best(row["INFOBLOX_TABLE"])
-                if not new_node:
-                    curr_node = left_Tree.add(row["INFOBLOX_TABLE"])
-                    self.data.at[index, "SELF_OVERLAP_A"] = "NO_OVERLAP"
-                    curr_node.data["INDEX"] = index
-                else: 
-                    self.data.at[index, "SELF_OVERLAP_A"] = new_node.data["INDEX"]
+                try:
+                    curr_network = str(ip.ip_network(self.data["INFOBLOX_TABLE"][index]))
+                except ValueError:
+                    print(f"The row : { self.data['INFOBLOX_TABLE'][index] } is not in the correct format")
+                else:
+                    if curr_network:
+                        new_node = left_Tree.search_best(curr_network)
+                        if not new_node:
+                            curr_node = left_Tree.add(curr_network)
+                            self.data.at[index, "SELF_OVERLAP_A"] = "NO_OVERLAP"
+                            curr_node.data["INDEX"] = index
+                        else:
+                            self.data.at[index,"SELF_OVERLAP_A"] = new_node.data["INDEX"]
         
         for index, row in self.data.iterrows():
-            if self.data["ROUTING_TABLE"][index]:
-                new_node = right_Tree.search_best(row["ROUTING_TABLE"])
-                if not new_node:
-                    curr_node = right_Tree.add(row["ROUTING_TABLE"])
-                    self.data.at[index, "SELF_OVERLAP_B"] = "NO_OVERLAP"
-                    curr_node.data["INDEX"] = index
+            if self.data["ROUTING_TABLE"][index]: 
+                try:
+                    curr_network = str(ip.ip_network(self.data["ROUTING_TABLE"][index]))
+                except ValueError:
+                    print(f"The row : { self.data['ROUTING_TABLE'][index] } is not in the correct format")
                 else:
-                    self.data.at[index, "SELF_OVERLAP_B"] = new_node.data["INDEX"]
-                    # self.data.loc[self.data["ROUTING_TABLE"] == new_node.prefix, "SELF_OVERLAP_B"] = new_node.prefix
+                    if curr_network:
+                        new_node = right_Tree.search_best(curr_network)
+                        if not new_node:
+                            curr_node = right_Tree.add(curr_network)
+                            self.data.at[index, "SELF_OVERLAP_B"] = "NO_OVERLAP"
+                            curr_node.data["INDEX"] = index
+                        else:
+                            self.data.at[index, "SELF_OVERLAP_B"] = new_node.data["INDEX"]
                         
         
-        # # CROSS OVERLAP
+        # CROSS OVERLAP
         for index, row in self.data.iterrows():
             if self.data["ROUTING_TABLE"][index]:
-                new_node = left_Tree.search_best(row["ROUTING_TABLE"])
-                if not new_node:
-                    curr_node = left_Tree.add(row["ROUTING_TABLE"])
-                    self.data.at[index, "CROSS_OVERLAP"] = "NO_OVERLAP"
-                    curr_node.data["INDEX"] = index
+                try:
+                    curr_network = str(ip.ip_network(self.data["ROUTING_TABLE"][index]))
+                except ValueError:
+                    print(f"The row : { self.data['ROUTING_TABLE'][index] } is not in the correct format")
                 else:
-                    self.data.at[index, "CROSS_OVERLAP"] = new_node.data["INDEX"]
-                    # self.data.loc[self.data["ROUTING_TABLE"] ==
-                    #               new_node.prefix, "CROSS_OVERLAP"] = new_node.prefix
+                    if curr_network:
+                        new_node = left_Tree.search_best(curr_network)
+                        if not new_node:
+                            curr_node = left_Tree.add(curr_network)
+                            self.data.at[index, "SELF_OVERLAP_B"] = "NO_OVERLAP"
+                            curr_node.data["INDEX"] = index
+                        else:
+                            self.data.at[index,"SELF_OVERLAP_B"] = new_node.data["INDEX"]
 
 
         self.data.to_csv("./Data/Output.csv")
+
 if __name__ == '__main__':
     start = time.perf_counter()
-    ip = IPAM()
-    ip.main()
+    _ipam = IPAM()
+    _ipam.main()
     end = time.perf_counter()
     print(f"The Whole script completed in {end-start:0.4f} seconds")
