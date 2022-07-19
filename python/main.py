1de99c54e7061e7d135712434b0b719a417a5311
Yuvi <yuvarajselva@hotmail.com>
2022-07-19 16:37:52 +0530

    feat: added the Indexing functionality

--- python/main.py
+++ python/main.py
@@ -16,32 +16,38 @@ class IPAM:
                 new_node = left_Tree.search_best(row["INFOBLOX_TABLE"])
                 if not new_node:
                     curr_node = left_Tree.add(row["INFOBLOX_TABLE"])
-                    self.data.at[index, "SELF_OVERLAP_A"] = "NO"
+                    self.data.at[index, "SELF_OVERLAP_A"] = "NO_OVERLAP"
+                    curr_node.data["INDEX"] = index
                 else: 
-                    self.data.at[index, "SELF_OVERLAP_A"] = "YES"
+                    self.data.at[index, "SELF_OVERLAP_A"] = new_node.data["INDEX"]
         
         for index, row in self.data.iterrows():
             if self.data["ROUTING_TABLE"][index]:
-                # print(self.data["ROUTING_TABLE"][index]) 
                 new_node = right_Tree.search_best(row["ROUTING_TABLE"])
                 if not new_node:
                     curr_node = right_Tree.add(row["ROUTING_TABLE"])
-                    self.data.at[index, "SELF_OVERLAP_B"] = "NO"
+                    self.data.at[index, "SELF_OVERLAP_B"] = "NO_OVERLAP"
+                    curr_node.data["INDEX"] = index
                 else:
-                    self.data.at[index, "SELF_OVERLAP_B"] = "YES"
+                    self.data.at[index, "SELF_OVERLAP_B"] = new_node.data["INDEX"]
+                    # self.data.loc[self.data["ROUTING_TABLE"] == new_node.prefix, "SELF_OVERLAP_B"] = new_node.prefix
+                        
         
         # # CROSS OVERLAP
         for index, row in self.data.iterrows():
             if self.data["ROUTING_TABLE"][index]:
-                # print(self.data["ROUTING_TABLE"][index])
                 new_node = left_Tree.search_best(row["ROUTING_TABLE"])
                 if not new_node:
                     curr_node = left_Tree.add(row["ROUTING_TABLE"])
-                    self.data.at[index, "CROSS_OVERLAP"] = "NO"
+                    self.data.at[index, "CROSS_OVERLAP"] = "NO_OVERLAP"
+                    curr_node.data["INDEX"] = index
                 else:
-                    self.data.at[index, "CROSS_OVERLAP"] = "YES"
+                    self.data.at[index, "CROSS_OVERLAP"] = new_node.data["INDEX"]
+                    # self.data.loc[self.data["ROUTING_TABLE"] ==
+                    #               new_node.prefix, "CROSS_OVERLAP"] = new_node.prefix
 
-        self.data.to_csv("./Data/Output.csv", index=False)
+
+        self.data.to_csv("./Data/Output.csv")
 if __name__ == '__main__':
     ip = IPAM()
     ip.main()
