fcd5299751828d00ecfd41cac2dea0abc19f632c
Yuvi <yuvarajselva@hotmail.com>
2022-08-02 18:22:22 +0530

    feat: added the cross overlap for a n b

--- python/main.py
+++ python/main.py
@@ -59,13 +59,29 @@ class IPAM:
                         new_node = left_Tree.search_best(curr_network)
                         if not new_node:
                             curr_node = left_Tree.add(curr_network)
-                            self.data.at[index, "CROSS_OVERLAP"] = "NO_OVERLAP"
+                            self.data.at[index, "CROSS_OVERLAP_B_2_A"] = "NO_OVERLAP"
                             curr_node.data["INDEX"] = index
                         else:
-                            self.data.at[index,"CROSS_OVERLAP"] = new_node.data["INDEX"]
+                            self.data.at[index,"CROSS_OVERLAP_B_2_A"] = new_node.data["INDEX"]
+
+        for index, row in self.data.iterrows():
+            if self.data["INFOBLOX_TABLE"][index]:
+                try:
+                    curr_network = str(ip.ip_network(self.data["INFOBLOX_TABLE"][index]))
+                except ValueError:
+                    print(f"The row : { self.data['INFOBLOX_TABLE'][index] } is not in the correct format")
+                else:
+                    if curr_network:
+                        new_node = right_Tree.search_best(curr_network)
+                        if not new_node:
+                            curr_node = right_Tree.add(curr_network)
+                            self.data.at[index, "CROSS_OVERLAP_A_2_B"] = "NO_OVERLAP"
+                            curr_node.data["INDEX"] = index
+                        else:
+                            self.data.at[index,"CROSS_OVERLAP_A_2_B"] = new_node.data["INDEX"]
 
 
-        self.data.to_csv("./data/out/Output.csv")
+        self.data.to_csv("./Data/out/Output.csv")
 
 if __name__ == '__main__':
     start = time.perf_counter()
